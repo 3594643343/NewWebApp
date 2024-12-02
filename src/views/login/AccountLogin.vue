a9959b8abce79680fb3ec0b46d3b57b301c5e207
nfr <2240690872@qq.com>
2024-12-02 19:23:49 +0800

    更新

--- src/views/login/AccountLogin.vue
+++ src/views/login/AccountLogin.vue
@@ -109,58 +109,7 @@ const showUserProfile = async () => {
         </div>
       </el-card>
     <!-- </el-container> -->
-  </template>
-  
-<script lang="ts" setup>
-  import { ref} from 'vue'
-  import { useRouter } from 'vue-router';
-  import { userLoginService } from '@/api/user';
-
-  const router = useRouter();
-
-  const form = ref({
-  username: '',
-  password: '',
-  })
-
-  const useremail= ref('');
-  
-  const rules = {
-  username: [
-    { required: true, message: '请输入用户名', trigger: 'blur' },
-    { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
-  ],
-  password: [
-    { required: true, message: '请输入密码', trigger: 'blur' },
-    { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }
-  ],
-  }
-  
-  const handleAccountLogin = async() => {
-    try {
-    const result = await userLoginService({
-      userEmail: useremail.value,
-      userName: form.value.username,
-      userPassword: form.value.password,
-    });
-    console.log(result);
-    if (result) {
-      // 登录成功，跳转到主页
-      localStorage.setItem('token', result.data.token);
-
-      console.log("Login:", result.data.token);
-      console.log('Login successful');
-      router.push('/main');
-    } else {
-      console.log("error:", result); // 输出错误信息
-    }
-  } catch (error) {
-    console.error('登录失败:', error); // 捕获并处理错误
-  }
-  }
-  
-</script>
-  
+</template>
   
   
 <style scoped>
