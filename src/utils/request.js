79b6463f75acc983c67067cb82f1d9d3812b9274
JK0328 <1341606022@qq.com>
2024-12-07 16:27:31 +0800

    连接后端

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { ElMessage } from 'element-plus';
-const baseURL = 'http://localhost:8079';
+// const baseURL = 'http://localhost:8079';
+const baseURL = 'http://121.37.24.76:8079';
 
 
 const instance = axios.create({
